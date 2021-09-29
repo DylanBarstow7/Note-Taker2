d0a6bb52f8336b6dad65e88f826968fbe6f9c759
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-28 22:13:54 -0500

    initiated use of api & created listening port

--- app/index.js
+++ app/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import apiRouter from "./routes/api.js"
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static("app/public"));
+app.use(express.json());
+
+app.use('/api')
+
+app.listen(PORT, () => {
+  console.log(`Listening PORT: ${PORT}`);
+});
