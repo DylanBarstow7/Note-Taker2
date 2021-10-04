9d15e4ba074ba85d6816e9cc45cf00ccad5da073
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-03 23:46:09 -0500

    updated console.log phrase

--- app/index.js
+++ app/index.js
@@ -5,10 +5,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static("app/public"));
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening PORT: ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
